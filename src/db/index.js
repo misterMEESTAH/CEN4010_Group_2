928ebcfdaf6304389d67cd9d37b246e5f88c45ef
Barnes2197 <mauriceb2197@gmail.com>
2021-02-18 17:24:38 -0500

    Found a better way to add to catch error from db connection

--- src/db/index.js
+++ src/db/index.js
@@ -4,15 +4,12 @@ const mongoose = require('mongoose');
 //Reads from a .env file but defaults to the string on the right if .env variable doesn't exist
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/books_example';
 mongoose.Promise = Promise;
-const db = () => { 
-    try { mongoose.connect(MONGODB_URI, {useNewUrlParser: true,  useUnifiedTopology: true })
+const db = mongoose.connect(MONGODB_URI, {useNewUrlParser: true,  useUnifiedTopology: true })
             .then(() =>
                 console.log("Successfully connected to MongoDB")
             )
-    }catch(e){
-        console.log(e);
-        return;
-    }
-};
+            .catch((e) => {
+                console.log(`Connection failed: ${e}`)
+            })
 
-module.exports = db();
+module.exports = db;
