dfb7b2cdc9af25389c7a837f10be4bdbadf23315
Maurice Barnes <mauriceb2197@gmail.com>
2021-02-18 17:27:53 -0500

    Add books to db maurice (#4)
    * Downgraded mongoose version to get rid of warning and wrapped db initialization in a function
    
    * Found a better way to add to catch error from db connection

--- src/db/index.js
+++ src/db/index.js
@@ -1,12 +1,15 @@
+const { NotExtended } = require('http-errors');
 const mongoose = require('mongoose');
 
 //Reads from a .env file but defaults to the string on the right if .env variable doesn't exist
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/books_example';
-console.log(MONGODB_URI);
 mongoose.Promise = Promise;
-const db = mongoose.connect(MONGODB_URI, {useNewUrlParser: true})
+const db = mongoose.connect(MONGODB_URI, {useNewUrlParser: true,  useUnifiedTopology: true })
             .then(() =>
                 console.log("Successfully connected to MongoDB")
-            );
+            )
+            .catch((e) => {
+                console.log(`Connection failed: ${e}`)
+            })
 
 module.exports = db;
