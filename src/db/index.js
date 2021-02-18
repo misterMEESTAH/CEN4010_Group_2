c487e9979990511bb0cc1848a89ac4e18fcfad29
Barnes2197 <mauriceb2197@gmail.com>
2021-02-18 17:12:25 -0500

    Downgraded mongoose version to get rid of warning and wrapped db initialization in a function

--- src/db/index.js
+++ src/db/index.js
@@ -1,12 +1,18 @@
+const { NotExtended } = require('http-errors');
 const mongoose = require('mongoose');
 
 //Reads from a .env file but defaults to the string on the right if .env variable doesn't exist
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/books_example';
-console.log(MONGODB_URI);
 mongoose.Promise = Promise;
-const db = mongoose.connect(MONGODB_URI, {useNewUrlParser: true})
+const db = () => { 
+    try { mongoose.connect(MONGODB_URI, {useNewUrlParser: true,  useUnifiedTopology: true })
             .then(() =>
                 console.log("Successfully connected to MongoDB")
-            );
+            )
+    }catch(e){
+        console.log(e);
+        return;
+    }
+};
 
-module.exports = db;
+module.exports = db();
