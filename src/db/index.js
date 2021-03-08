379cf0ac3e7aad80cb2440ee7797aa9b26ec4f77
KimC <kimmyconnolly@gmail.com>
2021-03-08 16:04:22 -0500

    added mongo db connection

--- src/db/index.js
+++ src/db/index.js
@@ -2,7 +2,7 @@ const { NotExtended } = require('http-errors');
 const mongoose = require('mongoose');
 
 //Reads from a .env file but defaults to the string on the right if .env variable doesn't exist
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/books_example';
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://kconn029:cen4010@cluster1.nekbz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 mongoose.Promise = Promise;
 const db = mongoose.connect(MONGODB_URI, {useNewUrlParser: true,  useUnifiedTopology: true })
             .then(() =>
