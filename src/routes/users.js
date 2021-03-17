d0e28c985453a4dfffe01eb7b3136432728dc351
jcara035 <jcara035@fiu.edu>
2021-03-16 21:55:20 -0400

    added users in routes

---
+++ src/routes/users.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const router = express.Router()
+const signUpTemplateCopy = require('../models/SignUpModels')
+const bcrypt = require('bcrypt')
+
+router.post('/signup', async (request, response) =>{
+
+    const saltPassword = await bcrypt.genSalt(10)
+    const securePassword = await bcrypt.hash(request.body.password, saltPassword)
+
+
+    const signedUpUser = new signUpTemplateCopy({
+        fullName:request.body.fullName,
+        username:request.body.username,
+        email:request.body.email,
+        password:securePassword,
+        address:request.body.address,
+        nickname:request.body.nickname,
+        creditcard:request.body.creditcard,
+        shippingaddress:request.body.shippingaddress
+    })
+    signedUpUser.save()
+    .then(data =>{
+        response.json(data)
+    })
+    .catch(error =>{
+        response.json(error)
+    })
+})
+
+
+module.exports = router
