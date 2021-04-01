40cb81993de633c5e0c2be097afed4c3a123ee39
Jorge Careaga <jcare001@fiu.com>
2021-03-31 21:29:38 -0400

    committing wishlist.js and css updates

---
+++ client/client_app/src/components/CatalogProducts/CatalogProducts.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import './CatalogProducts.css';
+
+class CatalogProucts extends React.Component {
+  render() {
+
+    if (this.props.productList) {
+      this.productList = this.props.productList;
+    }
+    return (
+      <div id="CatalogProducts" className="col-md-9">
+        <div className="row">
+          {this.productList.map(product => {
+              return (
+                <div key={product.pk} className="col-md-4 col-lg-3 mt-3">
+                  <div className="card text-center">
+                    <div className="card-body ">
+                      <img
+                        className="img-thumbnail img-tumbnail-clean"
+                        src={product.image}
+                        alt={product.product_name}/>
+                      <h6 className="card-title mt-4">{product.brand_name}</h6>
+                      <h6 className="card-subtitle mb-2 text-muted">{product.product_name}</h6>
+                      <button onClick={this.props.addToWishList.bind(this, product.pk, product.stock_number)} className="btn btn-sm btn-outline-secondary mt-4">&#9825; Add</button>
+                    </div>
+                  </div>
+                </div>
+              )
+            })}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default CatalogProucts;
---
+++ client/client_app/src/components/Wishlist/Wishlist.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import './WishList.css';
+
+class WishList extends React.Component {
+    render(){
+        if (this.props.WishList) {
+            this.WishList = this.props.WishList;
+        }
+
+        return (
+            <div className="col-md-12">
+            <form onSubmit={this.props.showWishList}>
+            <label>
+                Name:
+                <input type="text" name="name" />
+            </label>
+            <input type="submit" value="Submit" />
+            </form>
+                <div className="row row-border pt-2 pb-2">
+                    <div className="col-md-2 col-lg-2 d-flex align-items-center">
+                        <h5>My WishList</h5>
+                    </div>
+                    {this.WishList.map(wish => { return (
+                    <div key={wish.pk} className="col-md-2 col-lg-2 mt-2 mb-2">
+                        <div className="card text-center">
+                            <div className="card-body ">
+                                <button onClick={this.props.deleteFromWishList.bind(this, wish.pk)} className="btn btn-sm btn-outline-secondary d-flex">&#10006;</button>
+                                <img className="img-thumbnail img-tumbnail-clean" src={wish.image} alt={wish.product_name} />
+                            </div>
+                        </div>
+                    </div>
+                    ) })}
+                </div>
+            </div>
+        )
+    }
+
+}
+
+export default WishList;
