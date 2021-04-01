505aabeee9c27b91cdf8990fd9376af4fba3f269
Jorge Careaga <jcare001@fiu.com>
2021-03-31 21:28:46 -0400

    fixed routing to wishlist.js

--- client/client_app/src/WishList.js
+++ client/client_app/src/WishList.js
@@ -1,22 +1,161 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import './App.css';
+
+import CatalogProducts      from './components/CatalogProducts/CatalogProducts.jsx';
+import CatalogFiltering     from './components/CatalogFiltering/CatalogFiltering.jsx';
+import PaginationToolbar    from './components/PaginationToolbar/PaginationToolbar.jsx';
+import Wishlist             from './components/Wishlist/Wishlist.jsx';
+import booksFromDB          from './load_books'
+
+class App extends Component {
+
+  state = {
+    productList: [],
+    wishList: [],
+    wishNumber : undefined
+  }
+
+  constructor(props) {
+    super(props);
+    this.showProductList();
+    this.showWishList()
+  }
+
+  showProductList = () => {
+    fetch('http://127.0.0.1:8000/api/showproductlist/')
+      .then(
+        (response) => {
+          return response.json()
+        })
+      .then(
+        (data) => {
+          this.setState({
+            productList: data
+          })
+        }
+      )
+  }
+
+  showWishList = () => {
+
  
-class WishList extends Component {
+    fetch('http://127.0.0.1:8000/api/showwishlist/')
+    
+      .then(
+        (response) => {
+          return response.json()
+        })
+      .then(
+        (data) => {
+          const wishListNew = data;
+          this.setState({
+            wishList: wishListNew
+          })
+        }
+      )
+  }
+
+  wishNumberHandler = (event) => {
+    fetch('http://127.0.0.1:8000/api/showwishlist/' + event.target.value)
+    
+    .then(
+      (response) => {
+        return response.json()
+      })
+    .then(
+      (data) => {
+        const wishListNew = data;
+        this.setState({
+          wishList: wishListNew
+        })
+      }
+    )
+
+  }
+
+  addToWishList = (pk, stock_number, e) => {
+    e.preventDefault();
+    //Add to database
+    const checkWishItem = this.state.wishList.findIndex((wish) => {
+      return wish.stock_number === stock_number;
+    });
+    if (checkWishItem === -1) {
+      const url = 'http://127.0.0.1:8000/api/wishlist/';
+      fetch(url, {
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+          method: "POST",
+          body: JSON.stringify({
+            'name': pk
+          })
+        })
+        .then((response) => {
+          return response.json();
+        })
+      //Add to state
+      const index = this.state.productList.findIndex((product) => {
+        return product.pk === pk;
+      });
+
+      const product = this.state.productList[index];
+
+      this.setState({
+        wishList: [...this.state.wishList, product]
+      })
+
+    }
+  }
+
+  deleteFromWishList = (pk, e) => {
+    e.preventDefault();
+    //Delete form database
+    const url = 'http://127.0.0.1:8000/api/wishlist/' + pk + '/';
+    fetch(url, {
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      method: "DELETE",
+      body: JSON.stringify({
+        'name': pk
+      })
+    })
+    //Delete form state
+    const indexToDelete = this.state.wishList.findIndex((product) => {
+      return product.pk === pk;
+    })
+
+    this.setState({
+      wishList: this.state.wishList.filter((_, i) => i !== indexToDelete)
+    });
+
+  }
+
   render() {
     return (
-      <div>
-        <h2>WishList</h2>
-        <p>Mauris sem velit, vehicula eget sodales vitae,
-        rhoncus eget sapien:</p>
-        <ol>
-          <li>Nulla pulvinar diam</li>
-          <li>Facilisis bibendum</li>
-          <li>Vestibulum vulputate</li>
-          <li>Eget erat</li>
-          <li>Id porttitor</li>
-        </ol>
+      <div className="container">
+        <div className="row mt-4">
+          <PaginationToolbar />
+        </div>
+        <div className="row mt-2">
+          <Wishlist 
+          wishList = {this.state.wishList}
+          deleteFromWishList = {this.deleteFromWishList}
+          wishNumber = {this.state.wishNumber}
+          />
+        </div>
+        <div className="row mt-2">
+          <CatalogFiltering />
+          <CatalogProducts
+          productList = {this.state.productList}
+          addToWishList ={this.addToWishList}
+          />
+        </div>
       </div>
     );
   }
 }
- 
-export default WishList;
+
+export default App;
