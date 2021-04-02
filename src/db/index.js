44cbb20157fbf42f15a97c46f89bb50911612b43
jcara035 <jcara035@fiu.edu>
2021-04-02 18:40:00 -0400

    My Profile page works but doesnt update localstorage

--- src/db/index.js
+++ src/db/index.js
@@ -2,7 +2,7 @@ const { NotExtended } = require('http-errors');
 const mongoose = require('mongoose');
 
 //Reads from a .env file but defaults to the string on the right if .env variable doesn't exist
-const MONGODB_URI = process.env.MONGODB_URI || 'put string here';
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://jcara035:moTDDqBzPLsPTiEp@cluster1.nekbz.mongodb.net/sprint1?retryWrites=true&w=majority';
 mongoose.Promise = Promise;
 const db = mongoose.connect(MONGODB_URI, {useNewUrlParser: true,  useUnifiedTopology: true })
             .then(() =>
