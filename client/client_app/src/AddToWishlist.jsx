222da6d54e4774d364c1b411121bd08aee436a4f
Barnes2197 <mauriceb2197@gmail.com>
2021-04-10 13:43:54 -0400

    fixed some stuff for wishlist

--- client/client_app/src/AddToWishlist.jsx
+++ client/client_app/src/AddToWishlist.jsx
@@ -1,24 +1,41 @@
 import React from "react";
 import './App.css';
 import updateUser from './updateUser'
-import {addToWishList} from './components/actions/cartActions'
-import { useDispatch } from 'react-redux'
-
 
 function AddToWishlist (book) {
-
-    const dispatch = useDispatch()
-
-    const addBook = () => {
-        dispatch(addToWishList(book))
+    const addBook = async () => {
+        if(localStorage.getItem('user') === null || !localStorage.getItem('user')){
+            localStorage.setItem('user', JSON.stringify({}))
+        }
+        console.log(book)
+        let user = JSON.parse(localStorage.getItem('user'));
+        if (!user['wishlist']) {
+            user['wishlist'] = []
+        }
+        let wishlist = user['wishlist'];
+        
+        let inWishlist = false;
+        for(let i = 0; i < wishlist.length; i++){
+            if(wishlist[i]['book']['title'] === book['book']['title']){
+                wishlist[i]['book']['quantity'] = wishlist[i]['book']['quantity'] + book['book']['quantity'];
+                console.log(wishlist[i]['book'])
+                inWishlist = true;
+            }
+        }
+        if(!inWishlist){
+            wishlist.push(book)
+        }
+        user['wishlist'] = wishlist
+        if(user['email']) {
+            user = await updateUser(user);
+        }
+        localStorage.setItem('user', JSON.stringify(user))
     }
     return(
     <div>
-        <button className="waves-effect waves-light btn" onClick={addBook}>Add To Wishlist</button>
+        <button className="waves-effect waves-light btn" onClick={() => addBook(book)}>Add To Wishlist</button>
     </div>
     );
 }
 
-
-
-export default (AddToWishlist);
+export default AddToWishlist;
